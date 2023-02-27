11a1fd5207347467360568e4e9db7d4450fcd843
student203 <student203@203-08>
2023-02-27 10:40:32 +0500

    Добавьте файлы проекта.

---
+++ PR1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PR1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] a = new int[100];
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                a[i] = rnd.Next(0, 1000);
+                listBox1.Items.Add(a[i]);
+            }
+        }
+    }
+}
